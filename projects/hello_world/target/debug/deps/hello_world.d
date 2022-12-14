d024a4abda2ef2d9308f6aa36ea91d17a961e0cd
h4ngman <antonio.asperilla.diaz@gmail.com>
2022-12-14 15:11:46 +0100

    rust hello world

---
+++ projects/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\User\Documents\rust\Rust-tutorial\projects\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\User\Documents\rust\Rust-tutorial\projects\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ projects/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\User\Documents\rust\Rust-tutorial\projects\hello_world\target\debug\hello_world.exe: C:\Users\User\Documents\rust\Rust-tutorial\projects\hello_world\src\main.rs
