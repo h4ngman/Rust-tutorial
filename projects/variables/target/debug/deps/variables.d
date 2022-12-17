e741c5a368f1169a07ab80640dd85569c6650d6c
h4ngman <antonio.asperilla.diaz@gmail.com>
2022-12-17 14:09:31 +0100

    creation of variables project

---
+++ projects/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\User\Documents\rust\Rust-tutorial\projects\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\User\Documents\rust\Rust-tutorial\projects\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\User\Documents\rust\Rust-tutorial\projects\variables\target\debug\variables.exe: C:\Users\User\Documents\rust\Rust-tutorial\projects\variables\src\main.rs
