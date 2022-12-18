96fc1fb0a6b33e95b2ce56a48060998b9edf1ff3
h4ngman <antonio.asperilla.diaz@gmail.com>
2022-12-18 13:11:47 +0100

    adding shadowing

---
+++ projects/variables_shadowing/target/debug/deps/variables_shadowing.d
@@ -0,0 +1,5 @@
+C:\Users\User\Documents\rust\Rust-tutorial\projects\variables_shadowing\target\debug\deps\variables_shadowing.exe: src\main.rs
+
+C:\Users\User\Documents\rust\Rust-tutorial\projects\variables_shadowing\target\debug\deps\variables_shadowing.d: src\main.rs
+
+src\main.rs:
---
+++ projects/variables_shadowing/target/debug/variables_shadowing.d
@@ -0,0 +1,1 @@
+C:\Users\User\Documents\rust\Rust-tutorial\projects\variables_shadowing\target\debug\variables_shadowing.exe: C:\Users\User\Documents\rust\Rust-tutorial\projects\variables_shadowing\src\main.rs
